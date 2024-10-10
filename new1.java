2cdece112436cb395c95f22606f4a0607fdecea6
AniketKanelkar <aniketkanelkar@gmail.com>
2024-10-10 16:41:35 +0530

    Added a new 2nd line comment

--- new1.java
+++ new1.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 //My first java program
+//hi there i am learning git version control
 class new1{
     public static void main(String[] args){
         System.out.println("Hello");
